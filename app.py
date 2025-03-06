36ad10e1c721a08faa8b619da7817cee4c69cdf1
IamSamk <kadam.samartha@yahoo.com>
2025-03-06 09:29:17 +0530

    Update app.py
    Samk changed port

--- app.py
+++ app.py
@@ -27,5 +27,6 @@ def analyze_linkedin():
     return jsonify({"message": "LinkedIn URL received", "url": linkedin_url})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+
 
